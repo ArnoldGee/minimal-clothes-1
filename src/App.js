28663bcd920183a2647e559e514deafa1367ec8b
Arnau Gómez <agomez1f@gmail.com>
2020-09-01 20:34:34 +0200

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          {' '}
+          {/*Switch means that, the moment it finds a match, it does not render anything else inside the tag.*/}
+          <Route path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
