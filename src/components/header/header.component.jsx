0503a1df87becee325d611e37f1ab4a943570abe
Arnau Gómez <agomez1f@gmail.com>
2020-09-03 10:13:29 +0200

    :sparkles: Added a toggable shopping cart with Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
@@ -6,7 +10,7 @@ import {auth} from '../../firebase/firebase.utils'
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo"/>
@@ -27,12 +31,15 @@ const Header = ({currentUser}) => (
         <div className='option'>{currentUser.displayName}</div>
         :null
       }
+      <CartIcon/>
     </div>
+    {!hidden?<CartDropdown/>:null}
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart:{hidden}}) => ({ // OMG we are destructuring nested values!!!!
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
