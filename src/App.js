7691060f5488b6bf0aa701c0e62707243a1f17d5
Arnau Gómez <agomez1f@gmail.com>
2020-09-03 14:01:15 +0200

    🎨Added link to the checkout page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, {Component} from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect'
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
-import {selectCurrentUser} from './redux/user/user.selectors'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
+import {selectCurrentUser} from './redux/user/user.selectors';
 
 class App extends Component {
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const {setCurrentUser} = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           setCurrentUser({
             id: snapShot.id,
-            ...snapShot.data()
+            ...snapShot.data(),
           });
         });
       }
@@ -48,7 +48,18 @@ class App extends Component {
           {/*Switch means that, the moment it finds a match, it does not render anything else inside the tag.*/}
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="./" />) : (<SignInAndSignUpPage />)}/>
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="./" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        <Route exact path="/checkout" component={CheckoutPage}/>
         </Switch>
       </div>
     );
@@ -56,14 +67,11 @@ class App extends Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
-})
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  currentUser: selectCurrentUser,
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './checkout.styles.scss'
+
+const CheckoutPage = () => (
+  <div className="checkout-page">
+    <h1>Checkout</h1>
+  </div>
+)
+export default CheckoutPage
