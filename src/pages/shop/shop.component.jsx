8438daa1814ce1ed00896a3602ad5a1a6581b5e9
Arnau Gómez <agomez1f@gmail.com>
2020-09-04 09:20:06 +0200

    ✨Added items display by individual collections, and fixed a bug about items displaying too wide

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/> {/*it allows us to access the collectionId parameter inside the match object, depending on what we put after the slash (/) in the route. for example, if we write shop/xarel·lo, the collectionId is the string "xarel·lo", inside match.params.collectionID */}
   </div>
 );
 
